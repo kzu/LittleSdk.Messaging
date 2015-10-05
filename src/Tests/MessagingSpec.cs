a1782a72f2abda508a9ae7fce8bf49000032f225
Daniel Cazzulino <daniel@cazzulino.com>
2015-10-05 01:27:52 -0300

    Update to restore solution before building submodule source

---
+++ src/Tests/MessagingSpec.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Tests
+{
+	public class MessagingSpec
+	{
+		[Fact]
+		public void ShowMessage ()
+		{
+			var messaging = new LittleSdk.Messaging.Messaging(new LittleSdk.LittleSdk());
+
+			messaging.Send ("Hello World");
+		}
+	}
+}
